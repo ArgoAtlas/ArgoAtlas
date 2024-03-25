7c4b0c9d979ad3f298dc7db1db6a0b5bf002b79b
bananaprotocol <bananaprotocol@protonmail.com>
2024-03-26 00:28:43 +0100

    Handle ship coordinate rendering using custom DOM elements

--- src/index.js
+++ src/index.js
@@ -13,8 +13,31 @@ const canvas = d3
   .node();
 const context = canvas.getContext("2d");
 
+const detachedContainer = document.createElement("custom");
+const dataContainer = d3.select(detachedContainer);
+
 const projection = d3.geoEquirectangular();
 const geoGenerator = d3.geoPath().projection(projection).context(context);
+
+function drawCanvas() {
+  const elements = detachedContainer.childNodes;
+
+  elements.forEach((d) => {
+    const node = d3.select(d);
+
+    context.beginPath();
+    context.fillStyle = node.attr("fillStyle");
+    context.arc(
+      node.attr("x"),
+      node.attr("y"),
+      node.attr("radius"),
+      node.attr("startAngle"),
+      node.attr("endAngle"),
+    );
+    context.fill();
+    context.closePath();
+  });
+}
 
 function updateMap(json) {
   projection.fitExtent(
@@ -25,26 +48,42 @@ function updateMap(json) {
     json,
   );
 
+  context.clearRect(0, 0, canvas.width, canvas.height);
   context.beginPath();
   geoGenerator({ type: "FeatureCollection", features: json.features });
   context.stroke();
   context.closePath();
 }
 
+function drawPoints(mmsi, message) {
+  const detachedShip = detachedContainer.querySelector(`[mmsi="${mmsi}"]`);
+  const coords = projection([message.Longitude, message.Latitude]);
+
+  if (detachedShip) {
+    const ship = d3.select(detachedShip);
+    ship.attr("x", coords[0]).attr("y", coords[1]).attr("fillStyle", "blue");
+    d3.json("countries.json").then((data) => updateMap(data));
+  } else {
+    dataContainer
+      .append("custom")
+      .classed("point", true)
+      .attr("mmsi", mmsi)
+      .attr("x", coords[0])
+      .attr("y", coords[1])
+      .attr("radius", 1.2)
+      .attr("startAngle", 0)
+      .attr("endAngle", 2 * Math.PI)
+      .attr("fillStyle", "red");
+  }
+  drawCanvas();
+}
+
 d3.json("countries.json").then((data) => updateMap(data));
 
-socket.onmessage = function (event) {
+socket.addEventListener("message", (event) => {
   const message = JSON.parse(event.data);
 
   if (message.MessageType === "PositionReport") {
-    const coords = projection([
-      message.MetaData.longitude,
-      message.MetaData.latitude,
-    ]);
-
-    context.beginPath();
-    context.fillStyle = "red";
-    context.arc(coords[0], coords[1], 1.2, 0, 2 * Math.PI);
-    context.fill();
+    drawPoints(message.MetaData.MMSI, message.Message.PositionReport);
   }
-};
+});
