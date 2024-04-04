8b6978314c4b0b4da086961c2d208337ef2937c9
bananaprotocol <bananaprotocol@protonmail.com>
2024-04-04 19:11:15 +0200

    Add basic zoom logic

--- src/index.js
+++ src/index.js
@@ -12,24 +12,32 @@ const geoGenerator = d3.geoPath().projection(projection).context(context);
 
 const countries = await d3.json(`${serverAddress}/countries`);
 
-async function drawCanvas() {
+async function drawCanvas(transform) {
   const response = await fetch(`${serverAddress}/ships`);
   const ships = await response.json();
 
-  updateMap(countries);
+  if (!transform) transform = d3.zoomIdentity;
+
+  updateMap(countries, transform);
 
   ships.forEach((ship) => {
     const coords = projection([ship.longitude, ship.latitude]);
 
     context.beginPath();
     context.fillStyle = "red";
-    context.arc(coords[0], coords[1], 1.2, 0, 2 * Math.PI);
+    context.arc(
+      coords[0] + transform.x,
+      coords[1] + transform.y,
+      1.2 * transform.k,
+      0,
+      2 * Math.PI,
+    );
     context.fill();
     context.closePath();
   });
 }
 
-function updateMap(json) {
+function updateMap(json, transform) {
   const width = window.innerWidth;
   const height = window.innerHeight;
 
@@ -45,11 +53,30 @@ function updateMap(json) {
   canvas.height = height;
 
   context.clearRect(0, 0, canvas.width, canvas.height);
+  context.save();
   context.beginPath();
+  if (transform) {
+    context.translate(transform.x, transform.y);
+    context.scale(transform.k, transform.k);
+  }
   geoGenerator({ type: "FeatureCollection", features: json.features });
   context.stroke();
   context.closePath();
+  context.restore();
 }
 
-updateMap(countries);
+function zoomed(event) {
+  drawCanvas(event.transform);
+}
+
+d3.select(canvas).call(
+  d3
+    .zoom()
+    .scaleExtent([1, 8])
+    .on("zoom", (event) => {
+      zoomed(event);
+    }),
+);
+
+zoomed(d3.zoomIdentity);
 d3.interval(drawCanvas, 1000);
