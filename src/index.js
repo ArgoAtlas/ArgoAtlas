982f754bb2b7e6372ddc9646b9825c78a74bc35a
bananaprotocol <bananaprotocol@protonmail.com>
2024-04-06 04:38:02 +0200

    Add automatic updates for ship points

--- src/index.js
+++ src/index.js
@@ -1,32 +1,41 @@
 import { Deck } from "@deck.gl/core";
 import { GeoJsonLayer, ScatterplotLayer } from "@deck.gl/layers";
 
+const serverAddress = "http://localhost:5000";
+
 const INITIAL_VIEW_STATE = {
   latitude: 0,
   longitude: 0,
   zoom: 2,
 };
 
-const geoJsonLayer = new GeoJsonLayer({
-  id: "map",
-  data: "http://localhost:5000/countries",
-  filled: true,
-  getLineColor: [255, 255, 255],
-});
-
-const scatterplotLayer = new ScatterplotLayer({
-  id: "points",
-  data: "http://localhost:5000/ships",
-  stroked: false,
-  filled: true,
-  getPosition: (d) => [d.longitude, d.latitude],
-  getFillColor: [255, 0, 0],
-  radiusMinPixels: 2,
-  radiusMaxPixels: 3,
-});
-
 const deckInstance = new Deck({
   initialViewState: INITIAL_VIEW_STATE,
   controller: true,
-  layers: [scatterplotLayer, geoJsonLayer],
 });
+
+function update() {
+  deckInstance.setProps({
+    layers: [
+      new ScatterplotLayer({
+        id: "points",
+        data: `${serverAddress}/ships`,
+        stroked: false,
+        filled: true,
+        getPosition: (d) => [d.longitude, d.latitude],
+        getFillColor: [255, 0, 0],
+        radiusMinPixels: 2,
+        radiusMaxPixels: 3,
+      }),
+      new GeoJsonLayer({
+        id: "map",
+        data: `${serverAddress}/countries`,
+        filled: true,
+        getLineColor: [255, 255, 255],
+      }),
+    ],
+  });
+
+  setTimeout(update, 1000);
+}
+update();
