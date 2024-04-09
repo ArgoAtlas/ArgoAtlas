42a4b7664c1b13a4cec68bca5d3410ea1ca1c927
Youssef Galal El-Din <ct277@stud.uni-heidelberg.de>
2024-04-09 20:01:13 +0200

    Replace d3 with deckgl/add onHover display

--- src/index.js
+++ src/index.js
@@ -1,72 +1,58 @@
+import { Deck } from "@deck.gl/core";
+import { GeoJsonLayer, ScatterplotLayer } from "@deck.gl/layers";
+
 const serverAddress = "http://localhost:5000";
 
-d3.select("#map").style("position", "relative");
-const canvas = d3.select("#map").append("canvas").node();
-const ctx = canvas.getContext("2d");
+const INITIAL_VIEW_STATE = {
+  latitude: 0,
+  longitude: 0,
+  zoom: 1,
+};
 
-canvas.width = 800;
-canvas.height = 600;
+const deckInstance = new Deck({
+  initialViewState: INITIAL_VIEW_STATE,
+  controller: true,
+});
 
-const svg = d3
-  .select("#map")
-  .append("svg")
-  .attr("width", canvas.width)
-  .attr("height", canvas.height)
-  .style("position", "absolute")
-  .style("top", "0px")
-  .style("left", "0px");
+function update() {
+  deckInstance.setProps({
+    layers: [
+      new ScatterplotLayer({
+        id: "points",
+        data: `${serverAddress}/ships`,
+        filled: true,
+        getPosition: (d) => [d.longitude, d.latitude],
+        getFillColor: [255, 0, 0],
+        radiusMinPixels: 2,
+        radiusMaxPixels: 3,
 
-d3.queue()
-  .defer(d3.json, "../dist/world.topojson") // data for countries
-  .await(ready);
-
-var projection = d3
-  .geoMercator()
-  .translate([canvas.width / 2, canvas.height / 2]) // centering
-  .scale(110);
-
-var path = d3.geoPath().projection(projection); // create path with projection -> aus Punkten Pfade machen und ausfüllen
-
-var g = svg.append("g"); // ============================================================================================================================================================================================================== mui importante
-
-function ready(error, data) {
-  console.log(data);
-  var countries = topojson.feature(data, data.objects.countries).features;
-
-  console.log(countries);
-
-  g.selectAll(".country") // Länder
-    .data(countries)
-    .enter()
-    .append("path")
-    .attr("class", "country")
-    .attr("d", path)
-    .append("title")
-    .text((d) => d.properties.name);
-
-  svg.call(
-    d3.zoom().on("zoom", () => {
-      // pan & zoom functionality
-      g.attr("transform", d3.event.transform);
-    }),
-  );
-}
-
-async function drawCanvas() {
-  const response = await fetch(`${serverAddress}/ships`);
-  const ships = await response.json();
-
-  ships.forEach((ship) => {
-    const coords = projection([ship.longitude, ship.latitude]);
-
-    ctx.beginPath();
-    ctx.fillStyle = "red";
-    ctx.arc(coords[0], coords[1], 0.5, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.closePath();
+        pickable: true,
+        onHover: ({ object, x, y }) => {
+          const el = document.getElementById("tooltip");
+          if (object) {
+            const { mmsi } = object;
+            el.innerHTML = `<h1>MMSI: ${mmsi}</h1>`;
+            el.style.display = "block";
+            el.style.opacity = 0.9;
+            el.style.left = x + "px";
+            el.style.top = y + "px";
+          } else {
+            el.style.opacity = 0.0;
+          }
+        },
+      }),
+      new GeoJsonLayer({
+        id: "map",
+        data: `${serverAddress}/countries`,
+        stroked: true,
+        filled: true,
+        getFillColor: [185, 117, 29, 101],
+        getLineColor: [0, 0, 0],
+        lineWidthMinPixels: 0.5,
+      }),
+    ],
   });
 
-  console.log("updated");
+  setTimeout(update, 1000);
 }
-
-d3.timer(drawCanvas, 1000);
+update();
