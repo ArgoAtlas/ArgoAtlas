5f097debc3e60bd1c3b05cbb954623a6d08083e4
Youssef Galal El-Din <ct277@stud.uni-heidelberg.de>
2024-04-23 14:56:04 +0200

    Update Map - now using MapboxOverlay

--- src/index.js
+++ src/index.js
@@ -1,58 +1,61 @@
-import { Deck } from "@deck.gl/core";
-import { GeoJsonLayer, ScatterplotLayer } from "@deck.gl/layers";
+import { MapboxOverlay } from "@deck.gl/mapbox";
+import { ScatterplotLayer } from "@deck.gl/layers";
+import { Map } from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
 
 const serverAddress = "http://localhost:5000";
 
-const INITIAL_VIEW_STATE = {
-  latitude: 0,
-  longitude: 0,
-  zoom: 1,
-};
-
-const deckInstance = new Deck({
-  initialViewState: INITIAL_VIEW_STATE,
-  controller: true,
+const map = new Map({
+  container: "map",
+  style: "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json",
+  center: [0.45, 51.47],
+  zoom: 11,
 });
 
-function update() {
-  deckInstance.setProps({
-    layers: [
-      new ScatterplotLayer({
-        id: "points",
-        data: `${serverAddress}/ships`,
-        filled: true,
-        getPosition: (d) => [d.longitude, d.latitude],
-        getFillColor: [255, 0, 0],
-        radiusMinPixels: 2,
-        radiusMaxPixels: 3,
+await map.once("load");
 
-        pickable: true,
-        onHover: ({ object, x, y }) => {
-          const el = document.getElementById("tooltip");
-          if (object) {
-            const { mmsi } = object;
-            el.innerHTML = `<h1>MMSI: ${mmsi}</h1>`;
-            el.style.display = "block";
-            el.style.opacity = 0.9;
-            el.style.left = x + "px";
-            el.style.top = y + "px";
-          } else {
-            el.style.opacity = 0.0;
-          }
-        },
-      }),
-      new GeoJsonLayer({
-        id: "map",
-        data: `${serverAddress}/countries`,
-        stroked: true,
-        filled: true,
-        getFillColor: [185, 117, 29, 101],
-        getLineColor: [0, 0, 0],
-        lineWidthMinPixels: 0.5,
-      }),
-    ],
-  });
+const deckOverlay = new MapboxOverlay({
+  interleaved: true,
+  layers: [
+    new ScatterplotLayer({
+      id: "points",
+      data: `${serverAddress}/ships`,
+      filled: true,
+      getPosition: (d) => [d.longitude, d.latitude],
+      getFillColor: [255, 0, 0],
+      radiusMinPixels: 2,
+      radiusMaxPixels: 3,
 
-  setTimeout(update, 1000);
-}
-update();
+      pickable: true,
+      onHover: ({ object, x, y }) => {
+        const el = document.getElementById("tooltip");
+        if (object) {
+          const { mmsi } = object;
+          el.innerHTML = `<h1>MMSI: ${mmsi}</h1>`;
+          el.style.display = "block";
+          el.style.opacity = 1;
+          el.style.left = x + "px";
+          el.style.top = y + "px";
+        } else {
+          el.style.opacity = 0.0;
+        }
+      },
+
+      onClick: ({ object }) => {
+        const el2 = document.getElementById("tooltip");
+        if (object) {
+          const { latitude, longitude, mmsi } = object;
+          el2.innerHTML = `Latitude: ${latitude} <br> Longitude: ${longitude} <br> MMSI: ${mmsi}`;
+          el2.style.display = "block";
+          el.style.opacity = 0.9;
+          el.style.left = "px";
+          el.style.top = "px";
+        } else {
+          el.style.opacity = 0.0;
+        }
+      },
+    }),
+  ],
+});
+
+map.addControl(deckOverlay);
