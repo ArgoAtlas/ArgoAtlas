3bc6c6f4c57ddf55393bf70bdb4e84e3e1b1b6cd
Youssef Galal El-Din <ct277@stud.uni-heidelberg.de>
2024-04-23 14:52:16 +0200

    Update webpack for  mablibre-gl.css import

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
 const path = require("path");
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        loader: "css-loader",
+        options: {
+          modules: true,
+        },
+      },
+    ],
+  },
+
   mode: "development",
   entry: "./src/index.js",
   devServer: {
