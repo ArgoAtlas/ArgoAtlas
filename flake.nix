6b7b8300428b6f4f33c0dee8ccdf1580d8c79c0c
bananaprotocol <bananaprotocol@protonmail.com>
2024-09-05 12:45:14 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "A very basic flake";
+
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = with pkgs;
+        mkShell { buildInputs = [ nodejs ]; };
+    };
+}
