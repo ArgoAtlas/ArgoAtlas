ec1b3120fa9870e598fc023df4ea9dca22e05143
bananaprotocol <bananaprotocol@protonmail.com>
2024-11-11 19:53:00 +0100

    Add bundling related fields to proximity graph schema

--- models/proximityGraph.js
+++ models/proximityGraph.js
@@ -3,6 +3,10 @@ import mongoose from "mongoose";
 const proximityGraphSchema = new mongoose.Schema({
   coords: [[Number]],
   neighbors: [String],
+  m1: [Number],
+  m2: [Number],
+  centroids: [[Number]],
+  group: Number,
 });
 
 proximityGraphSchema.index({ position: "2dsphere" });
