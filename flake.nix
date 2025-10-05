634b2deeee6d32e8bd2c48b0275a42929f406b35
bananaprotocol <bananaprotocol@protonmail.com>
2025-10-05 17:56:57 +0200

    chore(flake): update

--- flake.nix
+++ flake.nix
@@ -1,14 +1,19 @@
 {
   description = "Nix flake for ArgoAtlas";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShells.${system}.default = with pkgs;
-        mkShell { buildInputs = [ nodejs ]; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [ nodejs ];
+      };
     };
 }
